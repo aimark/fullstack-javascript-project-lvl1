234b46e473a83e254fa56bfe98ca42ab2b9c9dbf
Андрей Марк <markandreym@gmail.com>
2022-07-29 12:54:18 +0300

    Changed progression

--- games/progression.js
+++ games/progression.js
@@ -2,12 +2,11 @@ import readlineSync from 'readline-sync';
 import { getRandomNumber } from '../src/random-num.js';
 
 export const progressionGame = () => {
-  let ifRight = true;
-  console.log('What number is missing in the progression?');
   let myArray = [];
   let elem = 0;
-  let correctAnswer = 0;
   let randomNumberDelta = 0;
+  let ifRight = true;
+  console.log('What number is missing in the progression?');
   for (let i = 0; i < 3; i += 1) {
     if (i === 0) {
       randomNumberDelta = getRandomNumber(1, 10);
@@ -33,7 +32,7 @@ export const progressionGame = () => {
       }
     }
     const randomIndex = getRandomNumber(1, 10);
-    correctAnswer = myArray[randomIndex];
+    const correctAnswer = myArray[randomIndex];
     console.log(`Question: ${myArray.slice(0, randomIndex).join(' ')} .. ${myArray.slice(randomIndex + 1).join(' ')}`);
     const userAnswer = Number(readlineSync.question('Your answer: '));
     if (userAnswer === correctAnswer) {
